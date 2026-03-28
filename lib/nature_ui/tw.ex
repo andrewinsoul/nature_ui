a305f4975f6156c4831e4469dca9905870ac271a
Andrew (Nature) Okoye <andrewinsoul@gmail.com>
2026-03-28 12:10:36 +0100

    fix a bug in the tailwind merge helper

--- lib/nature_ui/tw.ex
+++ lib/nature_ui/tw.ex
@@ -1,5 +1,12 @@
 defmodule NatureUi.Tw do
-  def merge(classes) do
-    TailwindMerge.merge(classes, TailwindMerge.ClassGroup)
+  def merge(classes) when is_list(classes) do
+    classes
+    |> Enum.filter(& &1)
+    |> Enum.join(" ")
+    |> TailwindMerge.merge()
+  end
+
+  def merge(classes) when is_binary(classes) do
+    TailwindMerge.merge(classes)
   end
 end
